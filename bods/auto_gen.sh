f508b349a3cbbf2c289c5f86a83067aa5a9e9c88
I-Ju Lin <liniju826@gmail.com>
2023-04-13 02:50:37 -0400

    Add workload data

---
+++ bods/auto_gen.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for ((k=0; k<=100; k+=10)); do
+    for ((l=0; l<=100; l+=10)); do
+        OUTPUT="./workloads/createdata_K"${k}"_L"${l}".txt"
+        ./sortedness_data_generator -N 1000000 -K $k -L $l -o $OUTPUT -S 0 -a 1 -b 1 -P 0
+    done
+done
