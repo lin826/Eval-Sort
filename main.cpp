76bcb13c4a2869d93df361e4ab1eac603430786d
I-Ju Lin <liniju826@gmail.com>
2023-04-13 13:17:02 -0400

    Fix QuickSort

--- main.cpp
+++ main.cpp
@@ -115,20 +115,21 @@ void swap(int *a, int *b) {
     *b = t;
 }
 
+int partition(int array[], int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
 void quickSort(int array[], int low, int high) {
     // Reference: https://www.programiz.com/dsa/quick-sort
-    auto partition = [](int array[], int low, int high) {
-        int pivot = array[high];
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
-                i++;
-                swap(&array[i], &array[j]);
-            }
-        }
-        swap(&array[i + 1], &array[high]);
-        return (i + 1);
-    };
 
     if (low < high) {
         int pi = partition(array, low, high);
