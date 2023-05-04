548841fd60302615b60a93ceccf430b89ba5b84c
I-Ju Lin <liniju826@gmail.com>
2023-05-03 23:39:36 -0400

    Update shell for comprehensive analysis

--- experiment.sh
+++ experiment.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-OUTPUT="./output/result_kl_100_mac.csv"
+OUTPUT="./output/result_int.csv"
 
 # screen -S k-${k}-${ALGO} -dm bash -c ""
 g++ -std=c++11 -Wall ./src/main.cpp -o ./main.out
 
 clear_cache () {
-    # # Clean cache on Linux
-    # sync; echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a;
-    # Clean cache on MacOS
-    # https://stackoverflow.com/questions/28845524/echo-3-proc-sys-vm-drop-caches-on-mac-osx
-    # https://gist.github.com/dan-palmer/3082266
-    sync && sudo purge;
-    launchctl unload -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist;
-    rm /private/var/vm/swapfile*;
-    launchctl load /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist;
+    # Clean cache on Linux
+    sync; echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a;
+
+    # # Clean cache on MacOS
+    # # https://stackoverflow.com/questions/28845524/echo-3-proc-sys-vm-drop-caches-on-mac-osx
+    # # https://gist.github.com/dan-palmer/3082266
+    # sync && sudo purge;
+    # launchctl unload -w /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist;
+    # rm /private/var/vm/swapfile*;
+    # launchctl load /System/Library/LaunchDaemons/com.apple.dynamic_pager.plist;
 }
 
-declare -a ALGOS=("kl_sort" "radix_sort" "std_stable" "merge_sort" "quick_sort" "tim_sort" "insertion_sort")
+declare -a ALGOS=("kl_sort" "radix_sort" "std_stable" "merge_sort" "quick_sort" "tim_sort")
 for ALGO in "${ALGOS[@]}"
 do
     for k in 100 50 25 10 5 1; do
